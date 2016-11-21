ccdfc08e0eda2d9517e14bf5f8776fbcd123e46f
phstadler <hello@phstadler.com>
2016-11-21 14:46:58 +0100

    Added initial libgdx project

---
+++ desktop/src/com/blockhalde/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blockhalde.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blockhalde.Blockhalde;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Blockhalde(), config);
+	}
+}
