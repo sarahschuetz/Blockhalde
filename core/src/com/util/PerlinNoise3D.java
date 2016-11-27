73975df1af9de0e14bc4a14bfa6af86f49582567
Sarah Schütz <sarahschuetz@gmx.at>
2016-11-27 16:04:13 +0100

    ADD perlin noise class

---
+++ core/src/com/util/PerlinNoise3D.java
@@ -0,0 +1,17 @@
+package com.util;
+
+public class PerlinNoise3D {
+	
+	/**
+	 * 
+	 * @return double between 0.0 and 1.0
+	 */
+	public double perlin(double x, double y, double z) {
+		
+		
+		
+		
+		
+		return 0;
+	}
+}
