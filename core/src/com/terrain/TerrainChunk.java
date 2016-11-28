e97bd08ce1a306af99827fea783f62b51c8e635b
Parzival Legmaier <legmaier@gmx.at>
2016-11-28 14:53:54 +0100

    TerrainChunk test with seed.

--- core/src/com/terrain/TerrainChunk.java
+++ core/src/com/terrain/TerrainChunk.java
@@ -5,15 +5,15 @@ import com.util.noise.PerlinNoise3D;
 public class TerrainChunk implements Chunk {
 	
 	private final short[][][] blocks = new short[X_MAX][Y_MAX][Z_MAX];
-	private final PerlinNoise3D perlinNoise = new PerlinNoise3D();
+	private final PerlinNoise3D perlinNoise = new PerlinNoise3D("Heinzibert".hashCode());
 	
 	public TerrainChunk() {
 		for(int x = 0; x < X_MAX; x++) {
 			for(int y = 0; y < Y_MAX; y++) {
 				for(int z = 0; z < Z_MAX; z++) {
-					double noise = perlinNoise.calcPerlinAt(x, y, z);
+					double noise = perlinNoise.calcPerlinAt((double) x / X_MAX, (double) y / Y_MAX, (double) z / Z_MAX);
 					
-					if(noise < 0.3) {
+					if(noise < 0.67) {
 						blocks[x][y][z] = BlockType.AIR.getBlockId();
 					} else {
 						blocks[x][y][z] = BlockType.DIRT.getBlockId();
