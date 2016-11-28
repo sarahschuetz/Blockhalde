2c2a5462ce2c50fae9a10b13e36cff5257f9706b
urstruktur <s1310238052@students.fh-hagenberg.at>
2016-11-28 22:01:57 +0100

    added icon

--- desktop/src/com/blockhalde/desktop/DesktopLauncher.java
+++ desktop/src/com/blockhalde/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.blockhalde.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.blockhalde.Blockhalde;
@@ -7,6 +8,8 @@ import com.blockhalde.Blockhalde;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.addIcon("util/icon_128.png", FileType.Internal);
+		config.addIcon("util/icon_32.png", FileType.Internal);
 		new LwjglApplication(new Blockhalde(), config);
 	}
 }
