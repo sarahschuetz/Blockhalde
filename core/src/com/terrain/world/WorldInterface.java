798ba71d36ea8a54c1bb40ebe809ed08455937ec
Andreas Pointner <pointner.andy@gmail.com>
2016-11-29 13:43:33 +0100

    dishwasher frank's philosophical views

--- core/src/com/terrain/world/WorldInterface.java
+++ core/src/com/terrain/world/WorldInterface.java
@@ -7,4 +7,10 @@ public interface WorldInterface {
      * Returns the chunk that is located at the specified position in the world.
      */
 	Chunk getChunk(int xPosition, int zPosition);
+
+
+	//This is quite pointless
+	//Lets assume its not:
+	//The importance of this message is not that easy definable since its dependent on the users perception
+	//Having said that, its still pointless
 }
