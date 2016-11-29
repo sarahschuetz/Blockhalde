c61997bc7df3596d2562e325acc7ce9e0aedf520
Parzival Legmaier <legmaier@gmx.at>
2016-11-29 13:44:13 +0100

    merge tool test

--- core/src/com/terrain/world/WorldInterface.java
+++ core/src/com/terrain/world/WorldInterface.java
@@ -7,4 +7,7 @@ public interface WorldInterface {
      * Returns the chunk that is located at the specified position in the world.
      */
 	Chunk getChunk(int xPosition, int zPosition);
+	// TODO: get relative chunk
+	// TODO: Get all chunks
+	// TODO: Dimensions (How many chunks), Center
 }
