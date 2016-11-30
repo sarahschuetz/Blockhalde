b59195054ade851ee5cb9eacc5f0c502e8b48cf9
Philipp Stadler <hello@phstadler.com>
2016-11-30 17:46:42 +0100

    Added textures to BlockType, removed explicit block ID byte

--- core/src/com/terrain/block/BlockType.java
+++ core/src/com/terrain/block/BlockType.java
@@ -5,19 +5,55 @@ package com.terrain.block;
  * data range.
  */
 public enum BlockType {
-	AIR((byte) 0),
-	WATER((byte) 1),
-	DIRT((byte) 2),
-	STONE((byte) 3);
+	AIR(-1.0f, null, null, null),
+	WATER(-1.0f, null, null, null),
+	DIRT(0.1f, "dirt", "dirt", "dirt"),
+	STONE(0.5f, "stone", "stone", "stone"),
+	GRASS(0.1f, "grass_top", "grass_side", "dirt");
 	
+	public static BlockType fromBlockId(int id) {
+		return BlockType.values()[id];
+	}
 	
-	private byte blockId;
+	private String topTextureName;
+	private String bottomTextureName;
+	private String sideTextureName;
+	/**
+	 * Contains a value proportional to how easy a block type is to destroy.
+	 * 
+	 * 0.0f means it is destroyed instantly, 1.0f is the maximum sturdiness.
+	 * -1.0f is a special value that indicates that the block is indestructible.
+	 */
+	private float sturdiness;
 	
-	BlockType(byte id) {
-		blockId = id;
+	BlockType(float sturdiness, String topTex, String sideTex, String bottomTex) {
+		this.sturdiness = sturdiness;
+		this.topTextureName = topTex;
+		this.bottomTextureName = bottomTex;
+		this.sideTextureName = sideTex;
+	}
+	
+	public String getTopTextureName() {
+		return topTextureName;
+	}
+	
+	public String getBottomTextureName() {
+		return bottomTextureName;
+	}
+	
+	public String getSideTextureName() {
+		return sideTextureName;
 	}
 
 	public byte getBlockId() {
-		return blockId;
+		return (byte) ordinal();
+	}
+	
+	public float getSturdiness() {
+		return sturdiness;
+	}
+	
+	public boolean isDestructible() {
+		return sturdiness != -1.0f;
 	}
 }
