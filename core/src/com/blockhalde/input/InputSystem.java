5ce54cfeec4b9e7d6e3590ea137a8ef89016bea1
Paul Lonauer <Paul Lonauer>
2016-12-02 01:28:05 +0100

    implement alternative way to hide cursor

--- core/src/com/blockhalde/input/InputSystem.java
+++ core/src/com/blockhalde/input/InputSystem.java
@@ -4,6 +4,8 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.blockhalde.render.CameraSystem;
 
 public class InputSystem extends EntitySystem {
@@ -17,7 +19,17 @@ public class InputSystem extends EntitySystem {
 		cameraController = new PlayerVirtualController(c.getCam());
 		inputProcessor = new PhysicalInputProcessor(cameraController);
 		Gdx.input.setInputProcessor(inputProcessor);
-//		Gdx.input.setCursorCatched(true);
+		
+		Pixmap pm = new Pixmap(16, 16, Format.RGBA8888); 
+		Pixmap.setBlending(Pixmap.Blending.None); 
+		pm.setColor(0f,0f,0f,0f);
+		pm.fillRectangle(0, 0, 16, 16);
+		Gdx.input.setCursorImage(pm, 0, 0);
+		pm.dispose();
+		
+		//CursorCatched would allow the cursor to move over an unfocused window without disappearing.
+		//But it seems to cause rendering errors on some systems...
+		//Gdx.input.setCursorCatched(true);
 	}
 
 	@Override
