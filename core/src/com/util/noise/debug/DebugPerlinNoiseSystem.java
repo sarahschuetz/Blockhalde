e065df157f1499d24d3fff6bc4143c7c98dce66f
Sarah Schütz <sarahschuetz@gmx.at>
2016-12-07 17:55:33 +0100

    organize imports

--- core/src/com/util/noise/debug/DebugPerlinNoiseSystem.java
+++ core/src/com/util/noise/debug/DebugPerlinNoiseSystem.java
@@ -3,21 +3,11 @@ package com.util.noise.debug;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShaderProgram;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.blockhalde.render.CameraSystem;
-import com.terrain.world.WorldManagementSystem;
-import com.util.noise.PerlinNoise3D;
 
 public class DebugPerlinNoiseSystem extends EntitySystem {
 
--- core/src/com/util/noise/debug/NoiseImg.java
+++ core/src/com/util/noise/debug/NoiseImg.java
@@ -1,7 +1,6 @@
 package com.util.noise.debug;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
