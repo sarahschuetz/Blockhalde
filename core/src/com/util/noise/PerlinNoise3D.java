3779cfd3b036915ab630247f786c2ad080660965
Sarah Schütz <sarahschuetz@gmx.at>
2017-01-01 22:04:32 +0100

    Update noise so negative values are possible

--- core/src/com/util/noise/PerlinNoise3D.java
+++ core/src/com/util/noise/PerlinNoise3D.java
@@ -2,29 +2,11 @@ package com.util.noise;
 
 import java.util.Random;
 
-public class PerlinNoise3D {
+public class PerlinNoise3D extends Noise {
 	
 	public int repeat;
-
-	/**
-	 * Hash lookup table as defined by Ken Perlin. This is a randomly arranged
-	 * array of all numbers from 0-255 inclusive.
-	 */
-	private static final int[] permutation_table = { 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7,
-			225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62,
-			94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175,
-			74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92,
-			41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18,
-			169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124,
-			123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223,
-			183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253,
-			19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210,
-			144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184,
-			84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141,
-			128, 195, 78, 66, 215, 61, 156, 180 };
-
-	private final int[] p = new int[512]; // Doubled permutation to avoid overflow
 	
+	// for more random results
 	double offsetX;
 	double offsetY;
 	double offsetZ;
@@ -94,32 +76,44 @@ public class PerlinNoise3D {
 	
 	
 	/**
+	 * Calculate perlin noise at coordinates
 	 * 
 	 * @return double between 0.0 and 1.0
 	 */
 	public double calcPerlinAt(double x, double y, double z) {
+		
+//		if(x < 0 || y < 0 || z < 0) {
+//			throw new RuntimeException("Negative input for calcPerlinAt() is not possible! (x: " + x + ", y: " + y + ", z: " + z + ")");
+//		}
 		
 		// add random offset
 		x += offsetX;
         y += offsetY;
         z += offsetZ;
 
-		if (repeat > 0) { // If we have any repeat on, change the coordinates to their "local" repetitions
+		if (repeat > 0) { // If there is a repeat, change the coordinates to their "local" repetitions
 			x = x % repeat;
 			y = y % repeat;
 			z = z % repeat;
 		}
 		
+		int floorX = floor(x);
+		int floorY = floor(y);
+		int floorZ = floor(z);
+		
+		// Find unit cube containing the point
 		// & -> clamp between 0 and 255
-		int xi = (int) x & 255; // Calculate the "unit cube" that the point asked will be located in
-		int yi = (int) y & 255; // The left bound is ( |_x_|,|_y_|,|_z_| ) and the right bound is that
-		int zi = (int) z & 255; // plus 1. Next we calculate the location (from 0.0 to 1.0) in that cube.
+		int xi = floorX & 255; // Calculate the "unit cube" that the point asked will be located in
+		int yi = floorY & 255; // The left bound is ( |_x_|,|_y_|,|_z_| ) and the right bound is that
+		int zi = floorZ & 255; // plus 1. Next we calculate the location (from 0.0 to 1.0) in that cube.
 		
+		// Get relative xyz coordinates of the point within the cube
 		// floating difference
-		double xf = x - (int) x;
-		double yf = y - (int) y;
-		double zf = z - (int) z;
+		double xf = x - floorX;
+		double yf = y - floorY;
+		double zf = z - floorZ;
 		
+		// Compute fade curves for xyz
 		// used for interpolation
 		double u = fade(xf);
 	    double v = fade(yf);
@@ -156,21 +150,8 @@ public class PerlinNoise3D {
 	   
 	    y2 = lerp(x1, x2, v);
 	    
+	    // For convenience we bind the result to 0 - 1 (theoretical min/max before is [-1, 1])
 	    return (lerp(y1, y2, w) + 1) /2; 
-	}
-	
-	/**
-	 * 
-	 * Fade function as defined by Ken Perlin.
-     * This eases coordinate values so that they will ease towards integral values.  
-     * This ends up smoothing the final output.
-     * 
-     * 6t^5 - 15t^4 + 10t^3
-	 * 
-	 */
-	private static double fade(double t) {
-	
-		return t * t * t * (t * (t * 6 - 15) + 10);  
 	}
 	
 	/**
@@ -188,49 +169,6 @@ public class PerlinNoise3D {
 	    }
 	    
 	    return num;
-	}
-	
-	/**
-	 * 
-	 * Calculate the dot product of a randomly selected gradient vector and the 8 location vectors.
-	 * (alternate way of writing the original code from Perlin in a much more easy-to-understand way (and actually faster in many languages))
-	 * Source: http://riven8192.blogspot.com/2010/08/calculate-perlinnoise-twice-as-fast.html
-	 * 
-	 * @param hash last 4 bits of the hash value determine which vector is being used 
-	 * @param x represents part x of the location vector (that will be used for the dot product)
-	 * @param y represents part y of the location vector (that will be used for the dot product)
-	 * @param z represents part z of the location vector (that will be used for the dot product)
-	 * @return random vector from the following 12 vectors (1,1,0), (-1,1,0), (1,-1,0), (-1,-1,0), (1,0,1), (-1,0,1), (1,0,-1), (-1,0,-1), (0,1,1), (0,-1,1), (0,1,-1), (0,-1,-1)
-	 */
-	private static double grad(int hash, double x, double y, double z)
-	{
-	    switch(hash & 0xF)
-	    {
-	        case 0x0: return  x + y;
-	        case 0x1: return -x + y;
-	        case 0x2: return  x - y;
-	        case 0x3: return -x - y;
-	        case 0x4: return  x + z;
-	        case 0x5: return -x + z;
-	        case 0x6: return  x - z;
-	        case 0x7: return -x - z;
-	        case 0x8: return  y + z;
-	        case 0x9: return -y + z;
-	        case 0xA: return  y - z;
-	        case 0xB: return -y - z;
-	        case 0xC: return  y + x;
-	        case 0xD: return -y + z;
-	        case 0xE: return  y - x;
-	        case 0xF: return -y - z;
-	        default: return 0; // never happens
-	    }
-	}
-	
-	/**
-	 *  Linear Interpolate
-	 */
-	private static double lerp(double a, double b, double x) {
-	    return a + x * (b - a);
 	}
 	
 }
