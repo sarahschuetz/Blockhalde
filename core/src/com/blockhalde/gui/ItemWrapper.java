ead1c5b91986c38e1464298da9cba2a98b797905
Sarah Sauseng <sarahsauseng@Sarahs-MacBook-Pro.local>
2017-01-03 11:49:00 +0100

    Added item wrapper for son

---
+++ core/src/com/blockhalde/gui/ItemWrapper.java
@@ -0,0 +1,16 @@
+package com.blockhalde.gui;
+
+import java.util.ArrayList;
+
+public class ItemWrapper {
+	
+	// data wrapper for general itemtypes
+	public ArrayList<Item> itemtypes;
+	// data wrapper for inventory
+	public ArrayList<Item> inventory;
+
+	// needed for libgdx json data 
+	public ItemWrapper() {
+	}
+
+}
